39e3e75fbb874c29454bffa97298e337812d2148
type613 <type613@gmail.com>
2021-01-02 14:53:56 +0900

    MPU6887 supported.

--- src/main.cpp
+++ src/main.cpp
@@ -1,44 +1,82 @@
+#define M5STACK_MPU6886 
+
 #include <M5Stack.h>
-#include "imu/IMU.h"
+#ifndef M5STACK_MPU6886
+    #include "imu/IMU.h"
+#endif
 #include "ble/BLE.h"
 #include "ble/BLEData.h"
 #include "input/ButtonCheck.h"
 
-#define SERIAL_PRINT 0
+#define SERIAL_PRINT 1
 
-void printSerial(unsigned long t, const float a[], const float g[], const float m[], const float q[]);
-void printLcd(unsigned long t, const float a[], const float g[], const float m[], const float q[]);
+void printSerial(const float a[], const float g[], const float m[], const float q[]);
+void printLcd(const float a[], const float g[], const float m[], const float q[]);
 
+#ifndef M5STACK_MPU6886
 imu::IMU _imu;
+#endif
 ble::BLE _ble;
 input::ButtonCheck _button;
 
+float acc[3];
+float gyro[3];
+float mag[3];
+float quat[4];
+
 void setup() {
     M5.begin();
+    M5.Power.begin();
+#ifdef M5STACK_MPU6886
+    M5.IMU.Init();
+    for (int i=0; i<3; i++) { acc[i] = 0.0F; }
+    for (int i=0; i<3; i++) { gyro[i] = 0.0F; }
+    for (int i=0; i<3; i++) { mag[i] = 0.0F; }
+    for (int i=0; i<4; i++) { quat[i] = (i>0) ? 0.0F : 1.0F; }
+#endif
     M5.Lcd.fillScreen(BLACK);
     M5.Lcd.setTextColor(GREEN ,BLACK);
     M5.Lcd.setCursor(0, 0);
+    M5.Lcd.setTextSize(2);
+
     M5.Lcd.println("awaken...");
+#if SERIAL_PRINT
+    Serial.println("awaken...");
+#endif
     if (Wire.begin()) {
         Wire.setClock(400000UL); // i2c 400kHz
         M5.Lcd.println("i2c OK!");
+#if SERIAL_PRINT
+        Serial.println("i2c OK!");
+#endif
     } else {
         M5.Lcd.println("i2c NG!!!");
+#if SERIAL_PRINT
+        Serial.println("i2c NG!");
+#endif
     }
+#ifndef M5STACK_MPU6886
     if (_imu.Setup(50)) { // 50Hz
         M5.Lcd.println("imu OK!");
     } else {
         M5.Lcd.println("imu NG!!!");
     }
+#endif
     if (_ble.Initialize() && _ble.Start()) { // 50Hz
         M5.Lcd.println("ble OK!");
+#if SERIAL_PRINT
+        Serial.println("ble OK!");
+#endif
     } else {
         M5.Lcd.println("ble NG!!!");
+#if SERIAL_PRINT
+        Serial.println("ble NG!");
+#endif
     }
 #if SERIAL_PRINT
     //Serial.begin(115200);
-    Serial.println("serial OK!");
-    M5.Lcd.println("serial OK!");
+    Serial.println("serial OK!!!");
+    M5.Lcd.println("serial OK!!!!");
 #endif
     M5.Lcd.fillScreen(BLACK);
 }
@@ -68,6 +106,8 @@ void loop() {
         }
     }
     // IMU condition
+
+#ifndef M5STACK_MPU6886
     if (_imu.Update()) {
         ble::IMUData data;
         memcpy(&data.accX, _imu.getAcc(), sizeof(float)*3);
@@ -77,34 +117,55 @@ void loop() {
         _ble.GetNineAxisCharacteristic().setValue((uint8_t*)&data, BLE_IMU_DATA_LEN);
         _ble.GetNineAxisCharacteristic().notify();
 #if SERIAL_PRINT
-        printSerial(_imu.getTime(), _imu.getAcc(), _imu.getGyro(), _imu.getMag(), _imu.getQuat());
+        printSerial(_imu.getAcc(), _imu.getGyro(), _imu.getMag(), _imu.getQuat());
 #endif
-        printLcd(_imu.getTime(), _imu.getAcc(), _imu.getGyro(), _imu.getMag(), _imu.getQuat());
+        printLcd(_imu.getAcc(), _imu.getGyro(), _imu.getMag(), _imu.getQuat());
     }
+#else
+    ble::IMUData data;
+    M5.IMU.getGyroData(&gyro[0],&gyro[1],&gyro[2]);
+    M5.IMU.getAccelData(&acc[0],&acc[1],&acc[2]);
+    M5.IMU.getAhrsData(&mag[0],&mag[1],&mag[2]);
+    //TODO:quat calculate
+
+    memcpy(&data.accX, &acc, sizeof(float)*3);
+    memcpy(&data.gyroX, &gyro, sizeof(float)*3);
+    memcpy(&data.magX, &mag, sizeof(float)*3);
+    //memcpy(&data.quatW, &quat, sizeof(float)*4);
+    _ble.GetNineAxisCharacteristic().setValue((uint8_t*)&data, BLE_IMU_DATA_LEN);
+    _ble.GetNineAxisCharacteristic().notify();
+#if SERIAL_PRINT
+    printSerial(acc, gyro, mag, quat);
+#endif
+    printLcd(acc, gyro, mag, quat);
+#endif
      // device update
+    delay(1);
     M5.update();
 }
 
-void printLcd(unsigned long t, const float a[], const float g[], const float m[], const float q[]) {
-  M5.Lcd.setCursor(0, 0);
-  M5.Lcd.print("     x       y       z ");
-  M5.Lcd.setCursor(0,  24);
-  M5.Lcd.printf("% 6d  % 6d  % 6d     mg   \r\n",  (int)(1000*a[0]), (int)(1000*a[1]), (int)(1000*a[2]));
-  M5.Lcd.setCursor(0,  44);
-  M5.Lcd.printf("% 6d  % 6d  % 6d     o/s  \r\n", (int)(g[0]), (int)(g[1]), (int)(g[2]));
-  M5.Lcd.setCursor(0,  64);
-  M5.Lcd.printf("% 6d  % 6d  % 6d     mG    \r\n",  (int)(m[0]), (int)(m[1]), (int)(m[2]));
-
-  M5.Lcd.setCursor(0,  100);
-  M5.Lcd.print("   qw      qx      qy      qz ");
-  M5.Lcd.setCursor(0,  128);
-  M5.Lcd.printf(" %2.3f  % 2.3f  %2.3f  %2.3f   \r\n", q[0], q[1], q[2], q[3]);
+void printLcd(const float a[], const float g[], const float m[], const float q[]) {
+  M5.Lcd.setCursor(0, 20);
+  M5.Lcd.printf("%6.2f  %6.2f  %6.2f      ", g[0], g[1], g[2]);
+  M5.Lcd.setCursor(220, 42);
+  M5.Lcd.print(" o/s");
+  M5.Lcd.setCursor(0, 65);
+  M5.Lcd.printf(" %5.2f   %5.2f   %5.2f   ", a[0], a[1], a[2]);
+  M5.Lcd.setCursor(220, 87);
+  M5.Lcd.print(" G");
+  M5.Lcd.setCursor(0, 110);
+  M5.Lcd.printf(" %5.2f   %5.2f   %5.2f   ", m[0], m[1], m[2]);
+  M5.Lcd.setCursor(220, 132);
+  M5.Lcd.print(" degree");
+  M5.Lcd.setCursor(0, 154);
+  M5.Lcd.print("  qw     qx     qy     qz");
+  M5.Lcd.setCursor(0,  176);
+  M5.Lcd.printf(" %2.3f % 2.3f %2.3f %2.3f \r\n", q[0], q[1], q[2], q[3]);
 }
 
 #if SERIAL_PRINT
-void printSerial(unsigned long t, const float a[], const float g[], const float m[], const float q[]) {
+void printSerial(const float a[], const float g[], const float m[], const float q[]) {
     return;
-    Serial.print(t); Serial.print(",");
     Serial.print(a[0], 3); Serial.print(",");
     Serial.print(a[1], 3); Serial.print(",");
     Serial.print(a[2], 3); Serial.print(",");
